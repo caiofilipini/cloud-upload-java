d2e8a62192bdc7f6e627951f43879ab9ca9289bc
Caio Filipini <caiofilipini@gmail.com>
2011-10-07 14:04:39 -0300

    improved UploadProgress interface and added equals() and hashCode()

--- src/java/com/caiofilipini/upload/UploadProgress.java
+++ src/java/com/caiofilipini/upload/UploadProgress.java
@@ -3,11 +3,19 @@ package com.caiofilipini.upload;
 public class UploadProgress {
 	
 	private final Long total;
-	private final Long completed;
+	private Long completed;
 
 	public UploadProgress(Long total, Long completed) {
 		this.total = total;
 		this.completed = completed;
+	}
+	
+	public UploadProgress(Long total) {
+		this(total, 0L);
+	}
+
+	public void completedMore(Long increment) {
+		this.completed += increment;
 	}
 	
 	public Integer calculatePercentage() {
@@ -16,6 +24,30 @@ public class UploadProgress {
 		// is based in the Content-Length header (which includes other POST data),
 		// it is necessary.
 		return percentage > 100 ? 100 : percentage;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (other == null) {
+			return false;
+		}
+		if (!UploadProgress.class.equals(other.getClass())) {
+			return false;
+		}
+		UploadProgress otherProgress = (UploadProgress) other;
+		return this.total.equals(otherProgress.total)
+				&& this.completed.equals(otherProgress.completed);
+	}
+	
+	@Override
+	public int hashCode() {
+		int hash = 17;
+		hash = hash * 31 + total.intValue();
+		hash = hash * 37 + completed.intValue();
+		return hash;
 	}
 
 }
--- src/test/com/caiofilipini/upload/UploadProgressTest.java
+++ src/test/com/caiofilipini/upload/UploadProgressTest.java
@@ -2,8 +2,25 @@ package com.caiofilipini.upload;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class UploadProgressTest {
+
+	@Test
+	public void shouldStartWithZeroCompleted() {
+		UploadProgress progress = new UploadProgress(2048L);
+		Integer expectedPercentage = 0;
+		assertEquals(expectedPercentage, progress.calculatePercentage());
+	}
+	
+	@Test
+	public void shouldUpdateCompleted() {
+		UploadProgress progress = new UploadProgress(2048L);
+		progress.completedMore(205L);
+		
+		Integer expectedPercentage = 10;
+		assertEquals(expectedPercentage, progress.calculatePercentage());
+	}
 	
 	@Test
 	public void shouldCalculatePercentageCompleted() {
@@ -22,11 +39,59 @@ public class UploadProgressTest {
 	@Test
 	public void shouldReturn100EvenIfPercentageIsGreater() {
 		// This may seem a little awkward, but since total size
-		// is based in the Content-Length header (which includes other POST data),
+		// is based in the Content-Length header (which may include other POST data),
 		// it is necessary.
 		UploadProgress progress = new UploadProgress(42L, 44L);
 		Integer expectedPercentage = 100;
 		assertEquals(expectedPercentage, progress.calculatePercentage());
+	}
+
+	@Test
+	public void defineEquals() {
+		shouldReturnTrueForSameInstance();
+		shouldReturnTrueForEqualValues();
+		shouldReturnFalseForInstancesOfOtherClasses();
+		shouldReturnFalseForNull();
+	}
+
+	@Test
+	public void defineHashCode() {
+		shouldBeDifferentIfObjectsAreDifferent();
+		shouldBeTheSameIfObjectsAreEqual();
+	}
+
+	private void shouldBeDifferentIfObjectsAreDifferent() {
+		UploadProgress aProgress = new UploadProgress(42L, 21L);
+		UploadProgress anotherProgress = new UploadProgress(88L, 12L);
+		assertFalse(aProgress.hashCode() == anotherProgress.hashCode());
+	}
+	
+	private void shouldBeTheSameIfObjectsAreEqual() {
+		UploadProgress aProgress = new UploadProgress(42L, 21L);
+		UploadProgress anotherProgress = new UploadProgress(42L, 21L);
+		assertEquals(aProgress.hashCode(), anotherProgress.hashCode());
+	}
+	
+	private void shouldReturnTrueForSameInstance() {
+		UploadProgress aProgress = new UploadProgress(42L, 21L);
+		assertEquals(aProgress, aProgress);
+	}
+	
+	private void shouldReturnTrueForEqualValues() {
+		UploadProgress aProgress = new UploadProgress(42L, 21L);
+		UploadProgress anotherProgress = new UploadProgress(42L, 21L);
+		assertEquals(aProgress, anotherProgress);
+	}
+	
+	private void shouldReturnFalseForInstancesOfOtherClasses() {
+		UploadProgress aProgress = new UploadProgress(42L, 21L);
+		String notAProgress = "please return false!";
+		assertFalse(aProgress.equals(notAProgress));
+	}
+	
+	private void shouldReturnFalseForNull() {
+		UploadProgress aProgress = new UploadProgress(42L, 21L);
+		assertFalse(aProgress.equals(null));
 	}
 
 }
