0f85984cf7745a80b50a9c3fcab4d9308baa0163
Caio Filipini <caiofilipini@gmail.com>
2011-10-07 14:52:58 -0300

    forgot to add file

---
+++ src/java/com/caiofilipini/upload/ProgressNotFoundException.java
@@ -0,0 +1,11 @@
+package com.caiofilipini.upload;
+
+public class ProgressNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ProgressNotFoundException(String message) {
+		super(message);
+	}
+
+}
