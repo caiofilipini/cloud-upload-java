5dbe4de3a563864f659e7f35c55edc3de08e41ab
Caio Filipini <caiofilipini@gmail.com>
2011-10-06 21:23:48 -0300

    grouped 404 tests for StatusHandler

--- src/test/com/caiofilipini/upload/StatusHandlerTest.java
+++ src/test/com/caiofilipini/upload/StatusHandlerTest.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class StatusHandlerTest {
 
@@ -30,50 +31,62 @@ public class StatusHandlerTest {
 	public void createRequestAndResponse() throws Exception {
 		request = mock(HttpServletRequest.class);
 		response = mock(HttpServletResponse.class);
-		
+
 		responseWriter = mock(PrintWriter.class);
 		when(response.getWriter()).thenReturn(responseWriter);
 	}
-	
+
 	@Test
-	public void shouldRespond404WhenUidIsNull() throws Exception {
-		when(request.getParameter("uid")).thenReturn(null);
-		verify404WasSent();
+	public void shouldRespond404() throws Exception {
+		whenUidIsNull();
+		whenUidIsEmpty();
+		whenUidContainsOnlyWhitespaces();
+		whenNoUploadProgressIsFoundForUid();
 	}
-	
-	@Test
-	public void shouldRespond404WhenUidIsEmpty() throws Exception {
-		when(request.getParameter("uid")).thenReturn("");
-		verify404WasSent();
-	}
-	
-	@Test
-	public void shouldRespond404WhenUidContainsOnlyWhitespaces() throws Exception {
-		when(request.getParameter("uid")).thenReturn("     ");
-		verify404WasSent();
-	}
-	
-	@Test
-	public void shouldRespond404WhenNoUploadProgressIsFoundForUid() throws Exception {
-		when(request.getParameter("uid")).thenReturn("41");
-		verify404WasSent();
-	}
-	
+
 	@Test
 	public void shouldRespond200WithPercentageAsJsonWhenUploadProgressIsAvailableForUid() throws Exception {
 		String uid = String.valueOf(System.currentTimeMillis());
 		InProgress.start(uid, new UploadProgress(1024L, 102L));
 		when(request.getParameter("uid")).thenReturn(uid);
-		
+
 		statusHandler.doGet(request, response);
-		
+
 		verify(response).setStatus(200);
 		verify(responseWriter).write("{\"completed\" : \"10\"}");
+	}
+
+	public void whenUidIsNull() throws Exception {
+		when(request.getParameter("uid")).thenReturn(null);
+		verify404WasSent();
+		reset();
+	}
+
+	public void whenUidIsEmpty() throws Exception {
+		when(request.getParameter("uid")).thenReturn("");
+		verify404WasSent();
+		reset();
+	}
+
+	public void whenUidContainsOnlyWhitespaces() throws Exception {
+		when(request.getParameter("uid")).thenReturn("     ");
+		verify404WasSent();
+		reset();
+	}
+
+	public void whenNoUploadProgressIsFoundForUid() throws Exception {
+		when(request.getParameter("uid")).thenReturn("41");
+		verify404WasSent();
+		reset();
 	}
 
 	private void verify404WasSent() throws ServletException, IOException {
 		statusHandler.doGet(request, response);
 		verify(response).sendError(404);
+	}
+
+	private void reset() {
+		Mockito.reset(request, response, responseWriter);
 	}
 
 }
