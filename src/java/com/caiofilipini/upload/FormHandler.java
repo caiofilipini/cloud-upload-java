41b0aa9e71f9d188109831d2eb20809dea6985ed
Caio Filipini <caiofilipini@gmail.com>
2011-10-07 15:11:03 -0300

    added FormHandler to render upload form

---
+++ src/java/com/caiofilipini/upload/FormHandler.java
@@ -0,0 +1,27 @@
+package com.caiofilipini.upload;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="FormHandler", urlPatterns="/form")
+public class FormHandler extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		long uid = System.currentTimeMillis();
+		
+		request.setAttribute("uid", uid);
+		response.setHeader("Content-Type", "text/html");
+		
+		request.getRequestDispatcher("WEB-INF/jsp/uploadForm.jsp").forward(request, response);
+	}
+
+}
---
+++ src/test/com/caiofilipini/upload/FormHandlerTest.java
@@ -0,0 +1,45 @@
+package com.caiofilipini.upload;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FormHandlerTest {
+
+	private HttpServletResponse response;
+	private HttpServletRequest request;
+	private FormHandler formHandler;
+
+	@Before
+	public void createSubject() {
+		formHandler = new FormHandler();
+	}
+	
+	@Before
+	public void createRequestAndResponse() {
+		RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+		response = mock(HttpServletResponse.class);
+		request = mock(HttpServletRequest.class);
+		when(request.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+	}
+
+	@Test
+	public void shouldGenerateUidAndForwardToForm() throws Exception {
+		formHandler.doGet(request, response);
+		
+		verify(request).setAttribute(eq("uid"), isNotNull());
+		verify(response).setHeader("Content-Type", "text/html");
+		verify(request).getRequestDispatcher("WEB-INF/jsp/uploadForm.jsp");
+	}
+
+}
